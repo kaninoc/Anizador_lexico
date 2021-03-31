7b12d5124a2d124b703075439d26dacbfa239f2e
kaninoc <47092141+kaninoc@users.noreply.github.com>
2021-03-30 21:31:28 -0500

    reservadas
    imprime primeros casos

--- lexico.py
+++ lexico.py
@@ -102,8 +102,11 @@ def idcomentarios(cadena):
         return True
     sp = cadena.find("*/")
     if sp != -1:
-        coment = False
-        return True
+        if coment == False:
+            return False
+        else:
+            coment = False
+            return True
     if coment == True:
         return True
 
@@ -120,27 +123,27 @@ def separarLinea(cadena):
 
     for i, letra in enumerate(cadena):
         if letra == ' ':
-            ##print("hola")
+            # print("hola")
             if palabraVar[0] != '':
                 if palabraVar[0].isnumeric():
                     resultados.append([int(palabraVar[0]), palabraVar[1]])
                 else:
                     resultados.append([palabraVar[0], palabraVar[1]])
                 palabraVar = ['', -1]
-            if simbolo[0]!='':
-                resultados.append([simbolo[0],simbolo[1]])
+            if simbolo[0] != '':
+                resultados.append([simbolo[0], simbolo[1]])
                 simbolo = ['', -1]
         if letra != ' ':
 
-            verificar_letra = letraValida.match(letra)##Letra o numero valido
-            ##print(bool(verificar_letra))
+            verificar_letra = letraValida.match(letra)  # Letra o numero valido
+            # print(bool(verificar_letra))
             if bool(verificar_letra):
-                ##print(bool(verificar_letra))
+                # print(bool(verificar_letra))
                 palabraVar[0] = palabraVar[0] + letra
                 if palabraVar[1] == -1:
                     palabraVar[1] = i+1
-                if simbolo[0]!='':
-                    resultados.append([simbolo[0],simbolo[1]])
+                if simbolo[0] != '':
+                    resultados.append([simbolo[0], simbolo[1]])
                     simbolo = ['', -1]
                 if i+1 == len(cadena):
                     if palabraVar[0].isnumeric():
@@ -149,14 +152,14 @@ def separarLinea(cadena):
                     else:
                         resultados.append([palabraVar[0], palabraVar[1]])
 
-            verificar_simbolo = simboloValido.match(letra)##simbolo valido
-            ##print(bool(verificar_simbolo))
+            verificar_simbolo = simboloValido.match(letra)  # simbolo valido
+            # print(bool(verificar_simbolo))
             if bool(verificar_simbolo):
-                simbolo[0] =simbolo[0] + letra
+                simbolo[0] = simbolo[0] + letra
                 if simbolo[1] == -1:
                     simbolo[1] = i+1
-                if palabraVar[0]!='':
-                    ##print("hola")
+                if palabraVar[0] != '':
+                    # print("hola")
                     if palabraVar[0].isnumeric():
                         resultados.append([int(palabraVar[0]), palabraVar[1]])
                     else:
@@ -165,25 +168,23 @@ def separarLinea(cadena):
                 if i+1 == len(cadena):
                     resultados.append([simbolo[0], simbolo[1]])
 
-
-
     return resultados
 
 
-# determina la existencia de palabras reservadas
-def analizador(lista, posiciones, linea):
-    print(lista)
-    for i, elemento in enumerate(lista):
-        try:
-            aux = reservadas.index(elemento)
-            if aux != -1:
-                print("<"+elemento+","+str(linea)+","+str(posiciones[i]+1)+">")
-        except ValueError:
-            print("<id,"+elemento+","+str(linea)+","+str(posiciones[i]+1)+">")
+# imprime y aniza coincidencias
+def analizador(lista, linea):
+    for elemento in lista:
+        if validar_reservada(elemento[0]):
+            print('<'+elemento[0]+','+str(linea)+','+str(elemento[1])+'>')
 
+def validar_reservada(elemento):
+    try:
+        n = reservadas.index(elemento)
+        return True
+    except ValueError:
+        return False
 
 # procedimiento principal
-
 
 while True:
 
@@ -191,8 +192,9 @@ while True:
         cadena = input()
 
         if idcomentarios(cadena) == False:
-            print(separarLinea(cadena))
-            # analizador(listpalabra, pospalabras,linea)
+            lineas_analizadas = separarLinea(cadena)
+            # print(lineas_analizadas)
+            analizador(lineas_analizadas, linea)
         linea += 1
     except EOFError:
         break
